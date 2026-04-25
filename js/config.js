30eeaccb1db48b0b4d013da5982b942cc53e3fde
B.MohanKrishna <159513894+mk1958@users.noreply.github.com>
2026-04-25 16:04:06 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@ window._config = {
     cognito: {
         userPoolId: 'ap-south-1:6adc6d35-3edf-42f2-ac80-89112aaef150', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '23cd42446nupcgmlulob8ie39s', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        region: 'ap-south-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
